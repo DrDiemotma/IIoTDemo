68853e18397905609cad9bd74eaea64d8678d3ad
DrDiemotma <matern.dierck@gmail.com>
2025-09-23 13:22:46 +0200

    [IIoTDemo #21] Logger initialization

--- DataCollectorDemo/UiNode/main.py
+++ DataCollectorDemo/UiNode/main.py
@@ -1,14 +1,19 @@
+import logging
+
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from UiNode.UiServer import UiServer
 from UiNode.routes import router
+import Common.Logging.setup
 
 ui_server: UiServer = UiServer()
 
 @asynccontextmanager
 async def lifespan(application: FastAPI):
+    Common.Logging.setup("UiServer", logging.INFO)
     await ui_server.register()
     yield
+    ui_server.shutdown()
 
 app = FastAPI(lifespan=lifespan)
 
